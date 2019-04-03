cd6ebe1d726499c0b02dc43188a159aa401dcec3
kaoru <679719+0x6b@users.noreply.github.com>
2019-04-04 00:48:57 +0900

    reduce text size for headings

--- src/heading.js
+++ src/heading.js
@@ -1,10 +1,16 @@
 export default function heading(turndownService) {
   turndownService.addRule("heading", {
     filter: ["h1", "h2", "h3", "h4", "h5", "h6"],
-    replacement: (content, node) => `
+    replacement: (content, node) => {
+      let level = 5 - Number(node.nodeName.charAt(1));
+      if (level < 1) {
+        level = 1;
+      }
+      return `
 
-[${"*".repeat(8 - Number(node.nodeName.charAt(1)))} ${content}]
+[${"*".repeat(level)} ${content}]
 
-`
+`;
+    }
   });
 }
